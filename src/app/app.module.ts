fae26feb3632c1a839d973b4172af4158687dc5c
leekeunha <leetoya@naver.com>
2021-02-05 16:50:16 +0900

    add homeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HomeModule } from './mall/home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
+    imports: [
+        BrowserModule, HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
