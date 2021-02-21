6470cad3ee3d531742cd9272382f2fb27f6be5f0
leekeunha <leetoya@naver.com>
2021-02-21 15:40:50 +0900

    add filterCategories in categoryProducts Page

--- src/app/client/fixture/data.js
+++ src/app/client/fixture/data.js
@@ -218,103 +218,13 @@ module.exports = function () {
                 "imageUrl": "/assets/images/products/Nike_Sportswear_Heritage_1.png",
                 "parentCategoryId": 5,
                 "categoryId": 1
-            },
-            {
-                "productId": 39,
-                "productName": "Nike Blazer Mid 77",
-                "soldOut": false,
-                "price": 120,
-                "numOfColors": 1,
-                "imageUrl": "/assets/images/home/latestGarment/Nike_Blazer_Mid_77_1.png",
-                "parentCategoryId": 1,
-                "categoryId": 1
-            },
-            {
-                "productId": 40,
-                "productName": "Nike Air Vapormax 2020 FlyKnit",
-                "soldOut": false,
-                "price": 220,
-                "numOfColors": 1,
-                "imageUrl": "/assets/images/home/latestGarment/Nike_Air_Vapormax_2020_FlyKnit_1.png",
-                "parentCategoryId": 1,
-                "categoryId": 1
-            },
-            {
-                "productId": 41,
-                "productName": "Nike Air Force 1 Shadow SE",
-                "soldOut": false,
-                "price": 120,
-                "numOfColors": 1,
-                "imageUrl": "/assets/images/home/latestGarment/Nike_Air_Force_1_ Shadow_SE_1.png",
-                "parentCategoryId": 1,
-                "categoryId": 1
-            },
-            {
-                "productId": 42,
-                "productName": "Nike Air Force 1",
-                "soldOut": false,
-                "price": 100,
-                "numOfColors": 1,
-                "imageUrl": "/assets/images/home/latestGarment/Nike_Air_Force_1_1.png",
-                "parentCategoryId": 1,
-                "categoryId": 1
-            },
-            {
-                "productId": 43,
-                "productName": "LeBron 18",
-                "soldOut": false,
-                "price": 200,
-                "numOfColors": 1,
-                "imageUrl": "/assets/images/home/latestGarment/LeBron_18_1.png",
-                "parentCategoryId": 1,
-                "categoryId": 1
-            },
-            {
-                "productId": 44,
-                "productName": "Nike Air Zoom SuperRep 2",
-                "soldOut": false,
-                "price": 120,
-                "numOfColors": 1,
-                "imageUrl": "/assets/images/home/latestGarment/Nike_Air_Zoom_SuperRep_2_1.png",
-                "parentCategoryId": 1,
-                "categoryId": 1
-            },
-            {
-                "productId": 45,
-                "productName": "Nike Metcon Sport",
-                "soldOut": false,
-                "price": 100,
-                "numOfColors": 1,
-                "imageUrl": "/assets/images/home/latestGarment/Nike_Metcon_Sport_1.png",
-                "parentCategoryId": 1,
-                "categoryId": 1
-            },
-            {
-                "productId": 46,
-                "productName": "Nike ZoomX SuperRep Surge",
-                "soldOut": false,
-                "price": 140,
-                "numOfColors": 1,
-                "imageUrl": "/assets/images/home/latestGarment/Nike_ZoomX_SuperRep_Surge_1.png",
-                "parentCategoryId": 1,
-                "categoryId": 1
-            },
-            {
-                "productId": 47,
-                "productName": "Nike React Infinity Run Flyknit 2",
-                "soldOut": false,
-                "price": 160,
-                "numOfColors": 1,
-                "imageUrl": "/assets/images/home/latestGarment/Nike_React_Infinity_Run_Flyknit_2_1.png",
-                "parentCategoryId": 1,
-                "categoryId": 1
             }
         ],
-        "parentCategories": [
+        "parentCategoryList": [
             {
                 "id": 1,
                 "name": "Shoes",
-                "categories": [
+                "categoryList": [
                     {
                         "categoryId": 1,
                         "categoryName": "Basketball"
@@ -340,7 +250,7 @@ module.exports = function () {
             {
                 "id": 2,
                 "name": "Tops",
-                "categories": [
+                "categoryList": [
                     {
                         "categoryId": 1,
                         "categoryName": "Short Sleeve Shirts"
@@ -358,7 +268,7 @@ module.exports = function () {
             {
                 "id": 3,
                 "name": "Pants",
-                "categories": [
+                "categoryList": [
                     {
                         "categoryId": 1,
                         "categoryName": "Sweatpants"
@@ -372,7 +282,7 @@ module.exports = function () {
             {
                 "id": 4,
                 "name": "Swimwear",
-                "categories": [
+                "categoryList": [
                     {
                         "categoryId": 1,
                         "categoryName": "Swim Bottoms"
@@ -386,7 +296,7 @@ module.exports = function () {
             {
                 "id": 5,
                 "name": "Accessories",
-                "categories": [
+                "categoryList": [
                     {
                         "categoryId": 1,
                         "categoryName": "Bags and Backpacks"
--- src/app/mall/categoryProducts/categoryProducts.componenet.ts
+++ src/app/mall/categoryProducts/categoryProducts.componenet.ts
@@ -1,7 +1,8 @@
-﻿import { Component, Input } from "@angular/core";
+﻿import { Component } from "@angular/core";
 import { ActivatedRoute } from '@angular/router';
 import { Repository } from '../../models/repository.model';
 import { Product } from '../../models/product.model';
+import { Category } from '../../models/category.model';
 
 @Component({
     selector: "category-products",
@@ -10,21 +11,29 @@ import { Product } from '../../models/product.model';
 })
 export class CategoryProductsComponent {
 
-    public cateogryProductList: Product[];
+    public productList: Product[];
+    public cateogryParentCategoryId: number;
     public productsPerPage = 9;
     public productsPerRow = 3;
     public selectedPage = 1;
+    public categoryList: Category[];
 
     constructor(private repository: Repository, activeRoute: ActivatedRoute) {
-        let category: string = activeRoute.snapshot.url[1].path;
+
+        let parentCategory: string = activeRoute.snapshot.url[1].path;
+
         repository.getProducts().subscribe(data => {
-            this.cateogryProductList = repository.getSelectedCategoryProductList(category);
+            this.productList = repository.getProductListInParentCategory(parentCategory);
+        });
+
+        repository.getParentCategories().subscribe(data => {
+            this.categoryList = repository.getCategoryListInParentCategory(parentCategory);
         });
     }
 
     get products(): Product[] {
         let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
-        return this.cateogryProductList.slice(pageIndex, pageIndex + this.productsPerPage);
+        return this.productList.slice(pageIndex, pageIndex + this.productsPerPage);
     }
 
     changePage(newPage: number) {
@@ -32,6 +41,10 @@ export class CategoryProductsComponent {
     }
 
     get pageCount(): number {
-        return Math.ceil(this.cateogryProductList.length / this.productsPerPage);
+        return Math.ceil(this.productList.length / this.productsPerPage);
+    }
+
+    getProductsByCategoryIdInParentCategory(categoryId:number) {
+        this.productList = this.repository.getProductsByCategoryIdInParent(categoryId);
     }
 }
